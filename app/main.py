dc8e63453564e355cd10a3486d311932a057b075
IUST_Enginner <157809957+mohammad-heidary@users.noreply.github.com>
2025-07-22 14:40:38 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ def favicon():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://687f2b87768f0515575892d2--supportchatbotai.netlify.app"],  
+    allow_origins=["https://687f5fd579ca75ea5df61836--supportchatbotai.netlify.app"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
